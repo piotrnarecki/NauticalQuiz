5063d9d9fac3726db4f8bfda0bff645c6e729785
piotrnarecki <nareckipiotraleksander@gmail.com>
2021-06-26 19:18:40 +0200

    added start page UI

--- app/src/main/java/pl/piotrnarecki/nauticalquiz/MainActivity.kt
+++ app/src/main/java/pl/piotrnarecki/nauticalquiz/MainActivity.kt
@@ -1,11 +1,28 @@
 package pl.piotrnarecki.nauticalquiz
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//hide status bar
+        if (Build.VERSION.SDK_INT < 16) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+            actionBar?.hide()
+        }
+        //
+
+
     }
 }
